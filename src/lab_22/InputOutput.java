9546135ee9b06d92eb99c6c526d70d92f2be293b
Alayode Auroraixion <chris@kayode.me>
2021-04-22 18:03:19 -0400

    added Lab InputOutput for java

---
+++ src/lab_22/InputOutput.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+
+public class InputOutput {
+    public static void main(String [] args) {
+        File foo = new File("input");
+        try {
+            Scanner input = new Scanner(foo);
+            String line = "";
+            String [] holder = new String [3];
+            int i = 0;
+            while(input.hasNext()) {
+                line = input.nextLine();
+                System.out.println(line);
+                line = line.replace("Java", "HTML");
+                holder[i]=line;
+                System.out.println(holder[i]);
+                i++;
+            } FileWriter fw = new FileWriter(foo);
+            PrintWriter replace = new PrintWriter(fw);
+            for(i = 0; i< 3; i++) {
+                replace.println(
+                        holder[i]);replace.flush();
+            }
+        } catch(IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
