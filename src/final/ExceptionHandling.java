9c75e3fe3b482195990c188f21ed252ff44081e6
Chris Samuel <Chris.samuel@auroraixion.com>
2021-05-17 00:14:53 +0100

    made name changes

--- src/final/ExceptionHandling.java
+++
@@ -1,4 +0,0 @@
-package final;
-
-
-class ExceptionHandling {}
---
+++ src/lastExam/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package lastExam;
+
+
+public class ExceptionHandling {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Prompt the user to eneter  two integers
+         System.out.print("Enter two integers: ");
+         int  number1 = input.nextInt();
+         int  number2 = input.nextInt();
+         System.out.println(number1 + "/" + number2 + " is " + (number1 /  number2));
+    }
+}
+
